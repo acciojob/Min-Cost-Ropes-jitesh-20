0b8398f134949e706ec6855e70e4ab77334238e3
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-16 13:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	arr=arr.sort();
+	let arr1=[];
+	let sum=0;
+	arr.forEach((item)=>{
+		sum+=item;
+		arr1.push(sum);
+	})
+	 sum=0;
+	arr1.forEach((item,index)=>{
+		if(index==0){
+			continue;
+		}else{
+		sum+=item;	
+		}
+	})
+	return sum;
 }
 
 module.exports=mincost;
